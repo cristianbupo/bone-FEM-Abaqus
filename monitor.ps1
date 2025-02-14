b44b723cef1f19a70385408e014810df0ef85c0f
Cristian Rodrigo Bustamante Porras <crbustamantep@unal.edu.co>
2025-02-14 18:44:02 -0500

    monitorFiles

---
+++ monitor.ps1
@@ -0,0 +1,36 @@
+param (
+    [string]$fileName = "current\analisis.log"
+)
+
+# Extract the actual file name from the full path
+$actualFileName = [System.IO.Path]::GetFileName($fileName)
+
+# Define a method to set the console title
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class ConsoleTitle {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool SetConsoleTitle(string lpConsoleTitle);
+}
+"@
+
+# Set the console title to include the file name
+[ConsoleTitle]::SetConsoleTitle("$actualfileName")
+
+# Wait for the specified file to be deleted and monitor the log file in the same window
+Write-Output "Monitoring $fileName..."
+
+while ($true) {
+    if (-not (Test-Path $fileName)) {
+        Clear-Host
+        Write-Output "$fileName deleted. Waiting for it to appear again..."
+        while (-not (Test-Path $fileName)) {
+            Start-Sleep -Seconds 1
+        }
+        Write-Output "$fileName appeared. Monitoring log file..."
+    }
+    Get-Content $fileName -Wait
+    Start-Sleep -Seconds 1
+}
