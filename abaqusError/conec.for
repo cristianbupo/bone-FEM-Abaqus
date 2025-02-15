c15ae3670b62a52fcd5632e46db23310f6c12faa
Cristian Rodrigo Bustamante Porras <crbustamantep@unal.edu.co>
2025-02-14 21:24:01 -0500

    cleanup

--- abaqusError/conec.for
+++
@@ -1,7 +0,0 @@
-      parameter(NUMNODE=591, NELEMS=550, dim=2, nnod=4)
-      real*8 nodes(NUMNODE, dim)
-      integer conectividades(NELEMS, nnod+1)
-      integer grupoFisico(NELEMS, 2)
-      common nodes
-      common conectividades
-      common grupoFisico
